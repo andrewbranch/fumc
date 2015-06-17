5197964782c8f4257a60cf7755ffa15680b52e47
Andrew Branch <andrewbranch@mail.com>
2015-06-17 19:22:05 -0400

    Add new fields to models

--- app/models/bulletin.js
+++ app/models/bulletin.js
@@ -3,6 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   date: DS.attr('date'),
   service: DS.attr('string'),
+  lectionary: DS.attr('string'),
+  liturgicalDay: DS.attr('string'),
   visible: DS.attr('boolean'),
-  file: DS.attr('string')
+  file: DS.attr('string'),
+  screenshot: DS.attr('string')
 });
--- app/models/witness.js
+++ app/models/witness.js
@@ -3,8 +3,10 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   from: DS.attr('date'),
   to: DS.attr('date'),
+  description: DS.attr('description'),
   volume: DS.attr('number'),
   issue: DS.attr('number'),
   visible: DS.attr('boolean'),
-  file: DS.attr('string')
+  file: DS.attr('string'),
+  screenshot: DS.attr('string')
 });
