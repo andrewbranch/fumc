c33ebeea296fd2d07fc973a96041246129f31ac5
Andrew Branch <andrewbranch@mail.com>
2015-08-02 23:07:13 -0700

    Use ember-cli-deploy for deployments

---
+++ config/deploy.js
@@ -0,0 +1,17 @@
+module.exports = {
+   production: {
+    store: {
+      type: 'S3',
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      bucket: 'fumc-mission-control',
+      hostName: 'fumc-mission-control.s3-website-us-east-1.amazonaws.com'
+    },
+    assets: {
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      bucket: 'fumc-mission-control',
+      exclude: ['index.html']
+    }
+  }
+};
